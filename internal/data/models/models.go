741b9fd249e607d6a80d63f221d9aca99d1c26e4
osamikoyo <v.pylayev@mail.ru>
2025-02-10 06:08:28 +0500

    add grpc and time parsing

--- internal/data/models/models.go
+++ internal/data/models/models.go
@@ -1,6 +1,12 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/osamikoyo/hrm-vocation/pkg/proto/pb"
+)
+
+const TIME_TAMPLATE = "2006.01.02"
 
 type Vocation struct{
 	UserID uint64
@@ -8,4 +14,17 @@ type Vocation struct{
 	DateEnd time.Time
 }
 
-func ToModels()
+func ToModels(voc *pb.Vocation) (*Vocation, error) {
+	startTime, err := time.Parse(TIME_TAMPLATE, voc.StartTime)
+	if err != nil{
+		return nil, err
+	}
+	endTime, err := time.Parse(TIME_TAMPLATE, voc.EndTime)
+	if err != nil{
+		return nil, err
+	}
+
+	return &Vocation{
+
+	}, nil
+}
